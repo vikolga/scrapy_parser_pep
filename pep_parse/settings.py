cd39b3b266405c6827e975f6bd6b16e3dc913a06
vikolga <ov.reatch@gmail.com>
2024-01-15 07:08:44 +0300

    второе ревью

--- pep_parse/settings.py
+++ pep_parse/settings.py
@@ -1,16 +1,16 @@
 from pathlib import Path
 
 
-BOT_NAME = "pep_parse"
+BOT_NAME = 'pep_parse'
 
-SPIDER_MODULES = ["pep_parse.spiders"]
-NEWSPIDER_MODULE = "pep_parse.spiders"
+SPIDER_MODULES = ['pep_parse.spiders']
+NEWSPIDER_MODULE = 'pep_parse.spiders'
 
 ROBOTSTXT_OBEY = True
 
-REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
-TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
-FEED_EXPORT_ENCODING = "utf-8"
+REQUEST_FINGERPRINTER_IMPLEMENTATION = '2.7'
+TWISTED_REACTOR = 'twisted.internet.asyncioreactor.AsyncioSelectorReactor'
+FEED_EXPORT_ENCODING = 'utf-8'
 
 
 ITEM_PIPELINES = {
